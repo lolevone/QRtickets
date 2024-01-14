bc4c48d6049b270169edeff44ac2c0311a1dd3cf
Shishikin Ivan Aleksandrovich <128998599+lolevone@users.noreply.github.com>
2024-01-14 17:33:24 +0300

    Add files via upload

---
+++ cypher.py
@@ -0,0 +1,73 @@
+def encrypt_text(text: str, gamma: list):
+    """Returns text encrypted using gamma encryption."""
+    new_text = ""
+    for symbol in text:
+        key = format(gamma[0], 'b')
+        if len(key) % 2 == 1:
+            key = '0' + key
+        symbol = format(ord(symbol), 'b')
+        while len(symbol) < 15:
+            symbol = '0' + symbol
+        new_symbol = ""
+        k = 0
+        for bit in symbol:
+            if bit != key[k]:
+                new_symbol += '1'
+            else:
+                new_symbol += '0'
+            k += 1
+            if k == len(key):
+                k = 0
+        new_symbol = chr(int(new_symbol, 2))
+        new_text += new_symbol
+        gamma[0] = ((gamma[0] * gamma[1]) % gamma[2])
+    return new_text  # .encode('utf-16', 'replace').decode('utf-16', 'replace')
+
+
+def encrypt_number(number: str, gamma: list):
+    """Returns a number encrypted using gamma encryption."""
+    new_number = ""
+    alphabet = "ABCDEF"
+    for symbol in number:
+        if symbol in alphabet:
+            for i in range(10, 16):
+                if symbol == alphabet[i - 10]:
+                    symbol = i
+                    break
+        else:
+            symbol = int(symbol)
+        key = format(gamma[0], 'b')
+        if len(key) % 2 == 1:
+            key = '0' + key
+        symbol = bin(symbol)[2:]
+        while len(symbol) < 4:
+            symbol = '0' + symbol
+        new_symbol = ""
+        k = 0
+        for bit in symbol:
+            if bit != key[k]:
+                new_symbol += "1"
+            else:
+                new_symbol += '0'
+            k += 1
+            if k == len(key):
+                k = 0
+        new_symbol = int(new_symbol, 2)
+        if new_symbol > 9:
+            new_symbol = alphabet[new_symbol - 10]
+        else:
+            new_symbol = str(new_symbol)
+        new_number += new_symbol
+        gamma[0] = ((gamma[0] * gamma[1]) % gamma[2])
+    return new_number
+
+
+def get_gamma_filename(name: str):
+    """Returns a list of values for gamma encryption calculated using the filename."""
+    initial_key = 0
+    factor = 1
+    for symbol in name:
+        initial_key += ord(symbol)
+        factor *= ord(symbol)
+    module = factor + initial_key
+    return [initial_key, factor, module]
---
+++ events.py
@@ -0,0 +1,153 @@
+import cypher
+import os
+
+path = ""
+list_of_events = []
+
+
+# Path and dir
+def set_path(new_path: str):
+    """Sets the working folder path and updates the list of events."""
+    global path
+    path = new_path
+    global list_of_events
+    list_of_events = get_list()
+
+
+def get_list():
+    """Returns a list of events located in the working folder."""
+    global path
+    list_of_files = []
+    for file in os.listdir(path):
+        if file[-4:] == ".txt":
+            if read_file_text(file[:-4])[:5] == "event":
+                list_of_files.append(file[:-4])
+    return list_of_files
+
+
+# Create and delete an event
+def new_event(name: str):
+    """Creates a new event with some name."""
+    global path
+    f = open(f"{path}{name}.txt", 'w', encoding="utf-8")
+    text = "event" + "\n\n" * 5
+    f.write(cypher.encrypt_text(text, cypher.get_gamma_filename(name)))
+    f.close()
+
+
+def remove_event(name: str):
+    """Deletes an event with the selected name."""
+    global path
+    os.remove(f"{path}{name}.txt")
+
+
+# File text
+def write_file_text(name: str, text: str):
+    """Writes text to a file using gamma cipher."""
+    global path
+    f = open(f"{path}{name}.txt", 'w', encoding="utf-8")
+    f.write(cypher.encrypt_text(text, cypher.get_gamma_filename(name)))
+    f.close()
+
+
+def read_file_text(name: str):
+    """Returns decrypted text from a file."""
+    global path
+    f = open(f"{path}{name}.txt", 'r', encoding="utf-8")
+    text = cypher.encrypt_text(f.read(), cypher.get_gamma_filename(name))
+    f.close()
+    return text
+
+
+# Events setters
+def set_title(name: str, title: str):
+    """Changes the event title."""
+    text = read_file_text(name).split("\n\n")
+    text[1] = title
+    text = "\n\n".join(text)
+    write_file_text(name, text)
+
+
+def set_description(name: str, description: str):
+    """Changes the event description."""
+    text = read_file_text(name).split("\n\n")
+    text[2] = description
+    text = "\n\n".join(text)
+    write_file_text(name, text)
+
+
+def set_time(name: str, start_time: str, end_time: str):
+    """changes the time of the event."""
+    text = read_file_text(name).split("\n\n")
+    text[3] = start_time + '/' + end_time
+    text = "\n\n".join(text)
+    write_file_text(name, text)
+
+
+def set_max_number_of_visitors(name: str, max_number_of_visitors: int):
+    """Changes the maximum number of event visitors."""
+    text = read_file_text(name).split("\n\n")
+    numbers = text[4].split('/')
+    numbers[1] = str(max_number_of_visitors)
+    numbers = '/'.join(numbers)
+    text[4] = numbers
+    text = "\n\n".join(text)
+    write_file_text(name, text)
+
+
+def set_audience_number(name: str, audience_number: str):
+    """Changes the location of the event."""
+    text = read_file_text(name).split("\n\n")
+    text[5] = audience_number
+    text = "\n\n".join(text)
+    write_file_text(name, text)
+
+
+# Events getters
+def get_title(name: str):
+    """Returns the event title."""
+    text = read_file_text(name).split("\n\n")
+    title = text[1]
+    return title
+
+
+def get_description(name: str):
+    """Returns the event description."""
+    text = read_file_text(name).split("\n\n")
+    description = text[2]
+    return description
+
+
+def get_time(name: str):
+    """Returns the time of the event. [start, end]"""
+    text = read_file_text(name).split("\n\n")
+    time = text[3].split('/')
+    return time
+
+
+def get_numbers_of_visitors(name: str):
+    """Returns the current and maximum number of visitors to an event. [current, max]"""
+    text = read_file_text(name).split("\n\n")
+    numbers = []
+    for number in text[4].split('/'):
+        numbers.append(int(number))
+    return numbers
+
+
+def get_audience_number(name: str):
+    """Returns the location of the event."""
+    text = read_file_text(name).split("\n\n")
+    audience_number = text[5]
+    return audience_number
+
+
+# Additional functions
+def add_visitor(name: str):
+    """Increases the number of current visitors by one."""
+    text = read_file_text(name).split("\n\n")
+    numbers = text[4].split('/')
+    numbers[0] = str(int(numbers[0]) + 1)
+    numbers = '/'.join(numbers)
+    text[4] = numbers
+    text = "\n\n".join(text)
+    write_file_text(name, text)
